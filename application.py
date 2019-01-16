bac8bfa702d8f521b12fa47908c7ed7d29ff66c9
Maythe Dirkzwager <maythe1508@gmail.com>
2019-01-16 10:51:07 +0000

    application

---
+++ application.py
@@ -0,0 +1,56 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import mkdtemp
+import pprint as pp
+
+from helpers import *
+
+# configure application
+app = Flask(__name__)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# custom filter
+app.jinja_env.filters["usd"] = usd
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+@app.route("/index", methods=["GET", "POST"])
+def index():
+    """Register user."""
+
+    # forget any user id
+    session.clear()
+
+    if request.method == "POST":
+
+        # nickname invullen
+        if not request.name.get("nickname"):
+            return apology("Must submit a nickname")
+
+        # knop spel maken
+
+        # knop spel joinen + geheime code invullen
+
+        # if invalid --> index_invalid_id.html
+
+
+
+    else:
+        return render_template("index.html")
